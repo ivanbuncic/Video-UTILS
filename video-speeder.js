c7a27fd447ea08769d828cbccb1d9ae468ff943a
Ivan Buncic <ivan@triplsi.com>
2018-08-31 22:52:13 +0200

    Create video-speeder.js
    Added speeder.

---
+++ video-speeder.js
@@ -0,0 +1,14 @@
+//# video-speeder
+// Video speeder is the one and only tool you need.
+
+// We don't have time for 1x speed. Even 2x is not enough. So let's watch 3 x the speed.
+
+// It works in any website with video. YouTube, Udemy, Frontend Masters...
+
+
+function speedMe() {
+    return document.getElementsByTagName("video")[0].playbackRate = 3;
+    };
+speedMe();
+
+You're welcome.
