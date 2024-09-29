43f69a4f6bc373e413638db155d0efdf3434b7d4
Ivan Buncic <ivan@triplsi.com>
2024-09-29 17:44:02 +0200

    Create changeBody.js

---
+++ changeBody.js
@@ -0,0 +1,8 @@
+// Copy some HTML that you need to paste
+// Open blank browser page if you are lazy to save the file, open code editor, start a dev server, if you just want to see what is in html and how it looks. 
+// Create a var with copied html
+
+const body = `PASTE YOUR HTML HERE (Watch for some characters that could potentially end the backtick template literal)`
+document.getElementsByTagName('body')[0].outerHTML = body;
+
+Voila, take a look at that beatiful html.
