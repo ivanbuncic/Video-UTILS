973f0726c216458b420a9e5deda85d4a3db22cd7
Ivan Buncic <ivan@triplsi.com>
2024-09-29 17:44:59 +0200

    Delete extracting-udemy-video-length.js

--- extracting-udemy-video-length.js
+++
@@ -1,3 +0,0 @@
-document.querySelectorAll('[data-purpose=\"course-additional-stats\"]')[0].children[1]./*  __reactEventHandlers$67hxu3mxhk2 this part is always changing, figure it out first manually */.children
-
-// You are more than welcome.
